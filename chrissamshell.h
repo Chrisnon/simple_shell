28b33f652ea55158ea23633102d77133bc8b615b
samie54 <126026530+samie54@users.noreply.github.com>
2023-05-24 08:49:32 -0700

    chrissamshell.h

--- chrissamshell.h
+++ chrissamshell.h
@@ -61,7 +61,6 @@ int _putchar(char c);
 void _puts(char *str);
 char *_strcpy(char *destn, char *src);
 int _isalpha(int c);
-int execute_command(char **args)
 int intlen(int num);
 int _atoi(char *s);
 char *_itoa(unsigned int n);
