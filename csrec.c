0d0be38059bb7a6ccbb7e62ef67e2a9a86df4dc4
samie54 <126026530+samie54@users.noreply.github.com>
2023-05-26 09:50:35 -0700

    csrec.c

--- csrec.c
+++ csrec.c
@@ -1,13 +1,30 @@
 #include "chrissamshell.h"
 
+/*********************************************************
+ * Project: ALX Simple Shell
+ *
+ *
+ * Team/Group/Collaboration Project
+
+ *
+ *
+ * Date: 15/05/2023
+ *
+ *
+ * Authors:
+ *        1. Samuel Atiemo
+ *        2. Christian Obi
+ *
+ **********************************************************/
+
 /**
- * history - write file with user inputs
- * @input: user input
- * Return: 0 on success -1 if failed
+ * csrec - writes characters withh user's inputs
+ * @input: user's character inputs.
+ * Return: 0, on success; -1 on fail.
  */
-int history(char *input)
+int csrec(char *input)
 {
-	char *filename = ".simple_shell_history";
+	char *filename = ".chrissam_shell_rec";
 	ssize_t fd, w;
 	int len = 0;
 
