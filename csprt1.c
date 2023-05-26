9bb4ca4777c6dced99badbde32ffd53b49e20e3a
samie54 <126026530+samie54@users.noreply.github.com>
2023-05-26 09:46:18 -0700

    csprt1.c

--- csprt1.c
+++ csprt1.c
@@ -1,7 +1,24 @@
 #include "chrissamshell.h"
 
+/*********************************************************
+ * Project: ALX Simple Shell
+ *
+ *
+ * Team/Group/Collaboration Project
+
+ *
+ *
+ * Date: 15/05/2023
+ *
+ *
+ * Authors:
+ *        1. Samuel Atiemo
+ *        2. Christian Obi
+ *
+ **********************************************************/
+
 /**
- * prompt - function that prints '$' to standard output
+ * csprompt - function that prints '$' to standard output
  */
 void prompt(void)
 {
