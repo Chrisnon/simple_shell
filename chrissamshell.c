097053c93a4b0bab3a5b27f57b29274735078c1c
samie54 <126026530+samie54@users.noreply.github.com>
2023-05-26 05:22:42 -0700

    chrissamshell.c

--- chrissamshell.c
+++ chrissamshell.c
@@ -1,5 +1,22 @@
 #include "chrissamshell.h"
 
+/*********************************************************
+ * Project: ALX Simple Shell
+ *
+ *
+ * Team/Group/Collaboration Project
+
+ *
+ *
+ * Date: 15/05/2023
+ *
+ *
+ * Authors:
+ *        1. Samuel Atiemo
+ *        2. Christian Obi
+ *
+ **********************************************************/
+
 /**
  * main - Entry point to program
  * @argc: Argument count
