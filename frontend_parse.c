b670b4bfab9622f68bfe00a7324dbb72974ef9b9
samie54 <126026530+samie54@users.noreply.github.com>
2023-05-26 14:33:22 -0700

    frontend_parse.c

--- frontend_parse.c
+++ frontend_parse.c
@@ -18,106 +18,35 @@
  **********************************************************/
 
 /**
- * frontend_parse - checks for parse commands.
- * @cmdd: commandd to be checked.
- * Return: 0,Success; -1 if fails.
+ * parse_cmd - Parses the command recieved from stdin
+ * @input: String gathered from stdin
+ * Return: Parsed strings to be used as arguments
  */
-int frontend_parse(char **cmdd)
+char **parse_cmd(char *input)
 {
-	frontend fun[] = {
-		{"cdd", NULL},
-		{"cs", NULL},
-		{"echo", NULL},
-		{"envi", NULL},
-		{"rec", NULL},
-		{NULL, NULL}
-	};
-	int i = 0;
+	char **arguments;
+	char *argument;
+	int i;
+	int buffsize = BUFSIZE;
 
-	if (*cmdd == NULL)
+	if (input[0] == ' ' && input[_strlen(input)] == ' ')
+		exit(0);
+	if (input == NULL)
+		return (NULL);
+	arguments = malloc(sizeof(char *) * buffsize);
+	if (!arguments)
 	{
-		return (-1);
+		free(arguments);
+		perror("hsh");
+		return (NULL);
 	}
-	while ((fun + i)->command)
+	argument = _strtok(input, "\n\t\r\a ");
+	for (i = 0; argument; i++)
 	{
-		if (_cmpcsstr(cmdd[0], (fun + i)->command) == 0)
-			return (0);
-		i++;
+		arguments[i] = argument;
+		argument = _strtok(NULL, "\n\t\r\a ");
 	}
-	return (-1);
+	arguments[i] = NULL;
+
+	return (arguments);
 }
-
-/**
- * cs_frontend - holds predefined buil-ins user commands.
- * @cmdd: arr of pasrsee commands.
- * @st: stringg of arr.
- * Return: 0 on success and -1 if fails.
- */
-int cs_frontend(char **cmdd, int st)
-{
-	cs cs_frontend[] = {
-		{"cdd", cs_dirr},
-		{"envi", cs_prompt},
-		{"cs", cs_prompt},
-		{"echo", cs_foo},
-		{"rec", rec_cs},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while ((cs_frontend + i)->command)
-	{
-		if (_cmpcsstr(cmdd[0], (cs_frontend + i)->command) == 0)
-		{
-			return ((cs_frontend + i)->function(cmdd, st));
-		}
-		i++;
-	}
-	return (-1);
-}
-
-/**
- * exitt_blt - exitt state for blt-in commands.
- * @cmdd: Arra of pasrse commands.
- * @input: free user's inputs.
- * @argv: Argsstarting programm || chrissamshell)
- * @c: count chrisamshell executes
- * @stat: Exitt state.
- */
-void exitt_blt(char **cmdd, char *input, char **argv, int c, int stat)
-{
-	int status, i = 0;
-
-	if (cmdd[1] == NULL)
-	{
-		free(input);
-		free(cmdd);
-		exit(stat);
-	}
-	while (cmdd[1][i])
-	{
-		if (_csalph(cmdd[1][i++]) != 0)
-		{
-			_prerror(argv, c, cmdd);
-			free(input);
-			free(cmdd);
-			exit(2);
-		}
-		else
-		{
-			status = _acs(cmdd[1]);
-			if (status == 2)
-			{
-				_prerror(argv, c, cmdd);
-				free(input);
-				free(cmdd);
-				exit(state);
-			}
-			free(input);
-			free(cmdd);
-			exit(state);
-
-		}
-	}
-}
-
